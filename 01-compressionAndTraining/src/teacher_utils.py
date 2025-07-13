a4e2cede9519ee8b4ddfb1b3e6e01a039d67c60b
ro <mromy00@gmail.com>
2025-07-13 22:44:15 +0200

    Solved  lr teacher training

--- 01-compressionAndTraining/src/teacher_utils.py
+++ 01-compressionAndTraining/src/teacher_utils.py
@@ -29,9 +29,13 @@ def train_teacher(bestHP, xTrain=None, xTest=None, yTrain=None, yTest=None,
     """
     Train the teacher model using the unified training function.
     """
-    
+    try:
+        lr = bestHP.get("learning_rate")
+    except:
+        lr = 1e-3  # fallback
+
     bestHP = bestHPBO_computation(bestHP, CONV_VAR, FC_VAR, UPPER_CONV, UPPER_FC)
-    lr = bestHP.get("learning_rate")
+  
     
     if DatasetConfig.D_SIGNAL == 1:
         model_fn = modelTeacherTopology_1D
